602a1119294d58897ef69bc966cb9cb69376b054
liuyali <2697721698@qq.com>
2021-04-22 11:07:40 +0800

    all

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <cat></cat>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import cat from './components/cat.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    cat
+  },
+})
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+html,body{
+  width: 100%;
+  height: 100%;
+}
+#app {
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px; */
+
+}
+</style>
---
+++ src/components/cat.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        {{aa}}
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+        
+    },
+    data(){
+        return {
+            aa:"aa"
+        }
+    },
+    mounted(){
+        this.aaa()
+    },
+    methods:{
+        aaa(){
+            var aa: string = "在在"
+            console.log(aa)
+        }
+    }
+})
+</script>
+<style scoped>
+
+</style>
